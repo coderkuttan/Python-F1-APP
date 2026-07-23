abffad3bfa9fde697620005600192fd279ad4217
Alan James <alanjam2006in@gmail.com>
2026-07-23 17:19:59 +0530

    Update streampy.py

--- streampy.py
+++ streampy.py
@@ -1,10 +1,34 @@
 import streamlit as st
 
-st.title("My First Streamlit App")
+# Title
+st.title("🔐 User Login Page")
 
-st.write("Hello, Welcome to Streamlit!")
+# User Role
+role = st.selectbox(
+    "Select User Role",
+    ["Student", "Faculty", "Admin"]
+)
 
-name = st.text_input("Enter your name")
+# Username
+username = st.text_input("Username")
 
-if st.button("Submit"):
-    st.success(f"Hello, {name}! Have a great day.")
+# Password
+password = st.text_input("Password", type="password")
+
+# Remember Me
+remember = st.checkbox("Remember Me")
+
+# Login Button
+if st.button("Login"):
+
+    if username == "alan" and password == "1234":
+        st.success(f"Welcome {username}!")
+        st.write("Role :", role)
+
+        if remember:
+            st.info("Your login will be remembered.")
+        else:
+            st.write("Remember Me is OFF")
+
+    else:
+        st.error("Invalid Username or Password")
